cbbee1e0e5cf69d7730db6a038f9e5d93e7bf683
Zoai <zoai@mac-mini.local>
2016-05-05 15:55:40 +0800

    1

---
+++ TTT/TTT/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  TTT
+//
+//  Created by Zoai on 16/5/5.
+//  Copyright © 2016年 Zoai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    NSString *str = @"l";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
